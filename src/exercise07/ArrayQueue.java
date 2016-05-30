b0c3836cc5b029a04ef89c62e7df82a762c7983a
Jannik Rahner <Jannik Rahner@DESKTOP-N1CKR4N3>
2016-05-30 20:24:00 +0200

    Exercise07: updated

--- src/exercise07/ArrayQueue.java
+++ src/exercise07/ArrayQueue.java
@@ -7,14 +7,14 @@ public class ArrayQueue<T> implements Queue<T>{
 	private int pointerLast=0;
 	private int pointerFirst=0;
 	
-	public ArrayQueue(int groesse) {
-		data = (T[]) new Object[groesse];
-		this.size=groesse;
+	public ArrayQueue(int size) {
+		data = (T[]) new Object[size];
+		this.size=size;
 	}
 	
 	@Override
 	public void enqueue(Object element) throws IllegalAccessException {
-		if(nextEmpty()==true)
+		if(nextEmpty())
 		{		
 			data[pointerLast]=(T)element;
 			
@@ -29,7 +29,7 @@ public class ArrayQueue<T> implements Queue<T>{
 
 	@Override
 	public T dequeue() {
-		T speicher=data[pointerFirst];
+		T dequeued=data[pointerFirst];
 		data[pointerFirst]=null;
 	
 		if(pointerFirst<size-1)
@@ -38,7 +38,7 @@ public class ArrayQueue<T> implements Queue<T>{
 		else
 			pointerFirst=0;
 
-		return speicher;
+		return dequeued;
 	}
 
 	@Override
@@ -48,14 +48,13 @@ public class ArrayQueue<T> implements Queue<T>{
 
 	@Override
 	public boolean empty() {
-		boolean empty=true;
-	
+		
 		for(int j=0;j<data.length;j++)
 		{
 			if(data[j]!=null)
-				empty=false;
+				return false;
 		}
-		return empty;
+		return true;
 	}
 	
 	public boolean nextEmpty()
